a50ca698f397595348dfd39260f10122978fbb67
Villermen <villermen@gmail.com>
2017-10-31 13:46:59 +0100

    More web3 test code

--- web3test.js
+++ web3test.js
@@ -1,11 +1,143 @@
 const Web3 = require("web3");
 
-var address = "0x30d93b122444912bea9a16da3bbec6e89686f24f";
+const web3 = new Web3("ws://localhost:8546");
 
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+console.log("Obtaining balance of coinbase...");
 
-web3.eth.getBalance(address)
-    .then(function (balance) {
+web3.eth.getCoinbase().then(function(coinbase) {
+    console.log("Coinbase: " + coinbase);
+    web3.eth.getBalance(coinbase).then(function (balance) {
         console.log("Balance: " + balance);
-    })
-    .catch(console.error);
+
+        obtainContractEvents();
+    }).catch(console.error);
+}).catch(console.error);
+
+
+function obtainContractEvents() {
+    console.log("Obtaining contract events...");
+    var beerContract = new web3.eth.Contract([
+        {
+            "constant": true,
+            "inputs": [],
+            "name": "active",
+            "outputs": [{"name": "", "type": "bool", "value": true}],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": false,
+            "inputs": [{"name": "_reference", "type": "string"}],
+            "name": "cancel",
+            "outputs": [],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [],
+            "name": "beerPrice",
+            "outputs": [{"name": "", "type": "uint256", "value": "1000"}],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": false,
+            "inputs": [],
+            "name": "toggleActive",
+            "outputs": [],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [],
+            "name": "orderLifetime",
+            "outputs": [{"name": "", "type": "uint256", "value": "604800"}],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": false,
+            "inputs": [],
+            "name": "drain",
+            "outputs": [],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "constant": false,
+            "inputs": [{"name": "_reference", "type": "string"}],
+            "name": "confirm",
+            "outputs": [],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "constant": false,
+            "inputs": [{"name": "_reference", "type": "string"}],
+            "name": "order",
+            "outputs": [],
+            "payable": true,
+            "stateMutability": "payable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "name": "_beerPrice",
+                    "type": "uint256",
+                    "index": 0,
+                    "typeShort": "uint",
+                    "bits": "256",
+                    "displayName": "&thinsp;<span class=\"punctuation\">_</span>&thinsp;beer Price",
+                    "template": "elements_input_uint",
+                    "value": "1000"
+                },
+                {
+                    "name": "_orderLifetime",
+                    "type": "uint256",
+                    "index": 1,
+                    "typeShort": "uint",
+                    "bits": "256",
+                    "displayName": "&thinsp;<span class=\"punctuation\">_</span>&thinsp;order Lifetime",
+                    "template": "elements_input_uint",
+                    "value": "604800"
+                }
+            ],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "constructor"
+        },
+        {
+            "anonymous": false,
+            "inputs": [
+                {"indexed": false, "name": "_reference", "type": "string"},
+                {"indexed": false, "name": "amount", "type": "uint256"}
+            ],
+            "name": "OrderCreated",
+            "type": "event"
+        },
+        {
+            "anonymous": false,
+            "inputs": [{"indexed": false, "name": "_reference", "type": "string"}],
+            "name": "OrderConfirmed",
+            "type": "event"
+        }
+    ], "0x345b63fcAA8fe182ad94564985edc0235EEC0ac4");
+
+    beerContract.events.OrderCreated({}, function (error, result) {
+        if (!error) {
+            console.log(result);
+        } else {
+            console.log(error);
+        }
+    });
+}
