520b3e30193714be66fc9cd94f71d674aab4e8e9
Luud van Keulen <luudvkeulen@gmail.com>
2017-11-08 11:08:08 +0100

    Hello world example.

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const port = process.env.port || 8080;
+
+app.get('/', (req, res) => res.send("Hello"));
+
+app.listen(port, () => console.log('Listening on port ', port));
