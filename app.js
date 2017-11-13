e42269dc6a8c92bd9ea67aa6df0e0d63387ec3ec
Luud van Keulen <luudvkeulen@gmail.com>
2017-11-13 11:13:28 +0100

    Added mongoose

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const port = process.env.port || 8080;
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send("Hello"));
 
