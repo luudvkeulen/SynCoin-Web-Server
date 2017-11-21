012c04239559688bfcaf782499816845cd38253d
Bram de Boer <bramdeboer@outlook.com>
2017-11-21 12:55:49 +0100

    getbalance controller

--- controllers/walletController.js
+++ controllers/walletController.js
@@ -5,37 +5,37 @@ exports.createWallet = function (req, res) {
 };
 
 exports.getTransactions = function (req, res) {
-    
+
 };
 
 exports.getBalance = function (req, res) {
-    syncoinService.getBalance(req.query.address).then( value => {
+    syncoinService.getBalance(req.query.address).then(value => {
         return res.status(200).send(value);
-      }, error => {
+    }, error => {
         return res.status(500).send(error);
-      });
+    });
 };
 
 exports.sendTransaction = function (req, res) {
-    syncoinService.sendTransaction(req.query.walletAddress, req.query.encryptedAccount, req.query.password, req.query.toAddress, req.query.amount).then( value => {
+    syncoinService.sendTransaction(req.query.walletAddress, req.query.encryptedAccount, req.query.password, req.query.toAddress, req.query.amount).then(value => {
         return res.status(200).send(value);
-      }, error => {
+    }, error => {
         return res.status(500).send(error);
-      });
+    });
 };
 
 exports.createOrder = function (req, res) {
-    
+
 };
 
 exports.cancelOrder = function (req, res) {
-    
+
 };
 
 exports.confirmOrder = function (req, res) {
-    
+
 };
 
 exports.drainOrder = function (req, res) {
-    
+
 };
