ebe0925f82c78aff22e98c3347771f72a6516f24
Luud van Keulen <luudvkeulen@gmail.com>
2017-11-28 15:30:28 +0100

    Added models for mongoose.

--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/UserController');
 
 router.post('/shop/order', (req, res) => {
     let email = req.body.email;
     let productId = req.body.productId;
-    if(!email || !productId) {
+    if (!email || !productId) {
         return res.sendStatus(500);
     }
 
+    userController.findByEmail(email);
+
     let dateTime = new Date().toLocaleString();
-    console.log(dateTime + " " + user + " " + orderProduct);
+    console.log(dateTime + " " + email + " " + productId);
+    res.sendStatus(200);
+});
+
+router.get('/shop/products', (req, res) => {
     res.sendStatus(200);
 });
 
