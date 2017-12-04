7dbb5861a9db2b47c0ddbcfdf9519d5b53859992
Bram de Boer <bramdeboer@outlook.com>
2017-12-04 12:35:05 +0100

    controller rewrite

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,63 @@
 const User = require('./../schemas/user');
 
+const jwt = require('jsonwebtoken');
+const { passport, jwtOptions } = require('./../jwt-config');
+
+const walletController = require('../controllers/WalletController');
+
+exports.login = function (req, res) {
+    const synCoinService = req.synCoinService;
+    const email = req.body.email;
+    const password = req.body.password;
+    walletController.findByEmail(email)
+        .then(wallet => {
+            if (synCoinService.verifyPassword(wallet.encryptedAccount, password)) {
+                const payload = { email: email };
+                const token = jwt.sign(payload, jwtOptions.secretOrKey);
+                return res.json({ token: token });
+            } else {
+                return res.status(400).send({ error: 'Incorrect password' });
+            }
+        })
+        .catch(error => {return res.status(500).send(error)});
+};
+
+exports.jwtTest = function (req, res) {
+    return res.json({ message: 'Success! You can not see this without a token' });
+};
+
+exports.register = function (req, res) {
+    let user = req.body;
+    if (!user.email
+        || !user.name
+        || !user.lastname
+        || !user.phone
+        || !user.company
+        || !user.address
+        || !user.password) {
+        return res.sendStatus(500);
+    }
+
+    create(
+            user.email,
+            user.name,
+            user.lastname,
+            user.phone,
+            user.company,
+            user.address)
+        .then(
+            walletController
+                .create(user.email, user.password)
+                .then(res.sendStatus(200))
+                .catch(() => {
+                        userController.remove(user.email);
+                        return res.sendStatus(500)
+                    }
+                )
+        )
+        .catch(error => { return res.sendStatus(500)});
+};
+
 function findByEmail(email) {
     return new Promise((resolve, reject) => {
         User.findOne({'email': email}, (error, result) => {
@@ -36,9 +94,3 @@ function create(email, name, lastname, phone, company, address) {
 function remove(email) {
     User.remove({email: email});
 }
-
-module.exports = {
-    findByEmail,
-    create,
-    remove
-};
