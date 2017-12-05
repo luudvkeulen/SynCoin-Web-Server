c624d8d6f4261f092f9bc4440b6701ccf6b5f978
Luud van Keulen <luudvkeulen@gmail.com>
2017-12-05 10:51:58 +0100

    Fixed errors: Routing in app.js removed and moved to routing. File location fixed in WalletController.

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const userRouter = require('./user');
+const walletRouter = require('./wallet');
 
+router.use(walletRouter);
 router.use(userRouter);
 
 module.exports = router;
